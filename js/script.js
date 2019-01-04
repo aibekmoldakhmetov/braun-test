44c9d908bafcca6d79812dedf84de57380544ca1
abu <sirabylai@gmail.com>
2019-01-04 20:34:03 +0600

    Первый скрипт для открытия и закрытия меню, начало верстки слайдера

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var menuBlock = document.getElementById('moblieMenuBlock');
+
+function openMenu() {
+	menuBlock.style.display = 'block';
+}
+
+function closeMenu() {
+	menuBlock.style.display = 'none';
+}
